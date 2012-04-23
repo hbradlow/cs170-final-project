aca514df33faa07cee2350df7875c8de4363885d
Henry Bradlow <hbradlow@berkeley.edu>
2012-04-23 14:07:27 -0700

    Added a web view of the graph solutions

---
+++ web/apps/solver/templatetags/solver_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils.safestring import mark_safe
+import json
+register = template.Library()
+
+@register.filter
+def graph(g=None):
+	from solver.solver import *
+	g = G
+	r = {"nodes":[],"links":[]}
+	i = 0
+	for n in g.adjacency_list():
+		r['nodes'].append({"nodeName":str(i),"group":"1","color":nodes.values()[i].color})
+		for l in n:
+			r['links'].append({"source":i,"target":l,"value":1})
+		i+=1
+	return mark_safe(json.dumps(r))
